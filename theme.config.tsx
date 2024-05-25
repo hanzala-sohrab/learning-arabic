a21fdecfe3b66f4435fe5406013fba72bc186953
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2024-05-25 20:34:28 +0530

    Update content

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Learning Arabic</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/hanzala-sohrab/learning-arabic',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/hanzala-sohrab/learning-arabic',
   footer: {
     text: 'Built using Next.js and Nextra Docs Template',
   },
